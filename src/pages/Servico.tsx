6ff4fc2b6986d61838c05a9224e74079d31f4278
AugustoMorais30 <augustomorais3011@gmail.com>
2022-09-23 06:48:22 -0300

    commit

--- src/pages/Servico.tsx
+++ src/pages/Servico.tsx
@@ -8,8 +8,7 @@ export const Servico = () => {
         <img src={img} alt="" className="" />
         <main>
              <h1>Serviço:</h1>
-             <p>Oferecemos suporte local ou remoto ilimitado, gerenciamento de servidor, rede e e-mail, backups, monitoramento e manutenção preventiva, segurança cibernética e muito mais.
-             Nossa equipe conta com analistas bilíngues/trilíngues (português, inglês e espanhol) prontos para atender suas necessidades.</p>
+             <p></p>
         </main>
         </>
     )
